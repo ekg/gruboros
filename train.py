e00212832c0af165165509da15c793391db965ec
Erik Garrison <erik.garrison@gmail.com>
2025-04-23 23:22:33 -0400

    trying to force untested optimizer into the mix

--- train.py
+++ train.py
@@ -885,6 +885,7 @@ def main():
                 "tp_grain_size": 64
             }
         },
+        "zero_allow_untested_optimizer": true,
         "zero_optimization": {
             "stage": 1,   # ZeRO stage 1 for DDP optimization
             "allgather_partitions": True,
