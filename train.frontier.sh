206c5e40d1360cbb8e36011a14a33480e650e568
Erik Garrison <erik.garrison@gmail.com>
2025-05-13 01:21:13 -0400

    test for 20 mins

--- train.frontier.sh
+++ train.frontier.sh
@@ -4,7 +4,7 @@
 #SBATCH -J minLM_frontier
 #SBATCH -o logs/minLM_frontier-%j.out
 #SBATCH -e logs/minLM_frontier-%j.err
-#SBATCH -t 01:00:00
+#SBATCH -t 00:20:00
 #SBATCH -p batch
 #SBATCH -N 128
 #SBATCH -q debug
