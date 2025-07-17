73d264f18d71cb59602dc741b723c95c791df0f3
Erik Garrison <erik.garrison@gmail.com>
2025-07-17 17:43:18 -0400

    for testing

--- train.frontier.sh
+++ train.frontier.sh
@@ -4,9 +4,9 @@
 #SBATCH -J minLM_gossip_srun_gloo
 #SBATCH -o logs/minLM_gossip-%j.out
 #SBATCH -e logs/minLM_gossip-%j.err
-#SBATCH -t 01:00:00
+#SBATCH -t 00:20:00
 #SBATCH -p batch
-#SBATCH -N 64
+#SBATCH -N 2
 #SBATCH -q debug
 #SBATCH --ntasks-per-node=8
 #SBATCH --gpus-per-node=8
