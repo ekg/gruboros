3fe7ea898d5d63f692bfe4f2cf8becf9e4ded8f2
Erik Garrison <erik.garrison@gmail.com>
2025-07-30 10:31:24 +0000

    chore: reduce chunk_size to 512 for 3g model memory efficiency

--- train.cuda.sh
+++ train.cuda.sh
@@ -68,7 +68,7 @@ deepspeed --num_gpus=$NUM_GPUS \
   --sf_beta2 0.999 \
   --weight_decay 0.0001 \
   --grad_accum 1 \
-  --chunk_size 1024 \
+  --chunk_size 512 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
