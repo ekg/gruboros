7f0a668da7291ff2a3b5856e935b0108833b8d68
Erik Garrison <erik.garrison@gmail.com>
2025-08-21 16:41:14 +0000

    Disable convolution by default: remove conv_kernel_size from training
    - minLM already defaults to conv_kernel_size=None (no conv)
    - Removed --conv_kernel_size 4 from train.lambda01.sh
    - Training will now run pure MinGRU without convolution layers
    - Conv can still be enabled by adding --conv_kernel_size N if needed

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -88,7 +88,6 @@ deepspeed --num_gpus=$NUM_GPUS \
   --filesystem-coordinator \
   --fitness-weighted-checkpointing \
   --elite-checkpoint-multiplier 20.0 \
-  --conv_kernel_size 4 \
   --cuda
 
 echo "Training finished."
