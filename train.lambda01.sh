3bc01d71da80aa9db43372acb83ff4616b24c533
Erik Garrison <erik.garrison@gmail.com>
2025-08-23 13:38:32 +0000

    Disable gradient clipping and use regular torch.compile
    - Set --grad_clip 0.0 to disable gradient clipping
    - Remove max-autotune mode from torch.compile
    - Use default compilation mode for faster startup

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -70,7 +70,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --sf_beta 0.9 \
   --sf_beta2 0.995 \
   --weight_decay 0.0001 \
-  --grad_clip 1.0 \
+  --grad_clip 0.0 \
   --grad_accum 64 \
   --chunk_size 256 \
   --batch_size 16 \
