27f1f6c32d9bab4a9fcaf20490011ee636b8bf05
Erik Garrison <erik.garrison@gmail.com>
2025-08-26 21:28:53 +0000

    Double batch size to 64 for maximum throughput

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -73,7 +73,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --grad_clip 0.0 \
   --grad_accum 32 \
   --chunk_size 256 \
-  --batch_size 32 \
+  --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
