9abc5f91f032dc603c2a8ee13a4facdf8ba89b68
Erik Garrison <erik.garrison@gmail.com>
2025-08-27 17:05:18 +0000

    Set chunk size to 16

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -72,7 +72,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --weight_decay 0.0001 \
   --grad_clip 1.0 \
   --grad_accum 8 \
-  --chunk_size 32 \
+  --chunk_size 16 \
   --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
