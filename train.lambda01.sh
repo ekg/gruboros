0e0393971fda56fd813bb6def34182fd7da2ee14
Erik Garrison <erik.garrison@gmail.com>
2025-08-27 09:22:58 +0000

    Halve chunk size to 128, double batch size to 128

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -72,8 +72,8 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --weight_decay 0.0001 \
   --grad_clip 1.0 \
   --grad_accum 32 \
-  --chunk_size 256 \
-  --batch_size 64 \
+  --chunk_size 128 \
+  --batch_size 128 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
