a4951dd20c410fb7442d8088a5c155bbc53b19d1
Erik Garrison <erik.garrison@gmail.com>
2025-08-28 15:23:53 +0000

    Optimize torch.compile and training parameters
    - Set torch.compile mode to "max-autotune" for maximum performance
    - Reverted grad_accum back to 8
    - Set chunk_size to 64 tokens

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -71,8 +71,8 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --sf_beta2 0.995 \
   --weight_decay 0.0001 \
   --grad_clip 0.0 \
-  --grad_accum 1 \
-  --chunk_size 256 \
+  --grad_accum 8 \
+  --chunk_size 64 \
   --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
