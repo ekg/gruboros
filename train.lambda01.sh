2c118fd64e856fe3afcae3b2eb2ca516987921f3
Erik Garrison <erik.garrison@gmail.com>
2025-08-28 16:25:40 +0000

    Increase chunk size to 256

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -72,7 +72,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --weight_decay 0.0001 \
   --grad_clip 0.0 \
   --grad_accum 8 \
-  --chunk_size 128 \
+  --chunk_size 256 \
   --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
