ae07de7410f04945604bf803426a9fdfec8950a3
Erik Garrison <erik.garrison@gmail.com>
2025-08-28 14:58:36 +0000

    Disable gradient clipping and increase chunk size to 256

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -70,9 +70,9 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --sf_beta 0.9 \
   --sf_beta2 0.995 \
   --weight_decay 0.0001 \
-  --grad_clip 1.0 \
+  --grad_clip 0.0 \
   --grad_accum 8 \
-  --chunk_size 64 \
+  --chunk_size 256 \
   --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
