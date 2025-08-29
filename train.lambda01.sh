1e151b707a725cd37bcdfb44dfee69554209bfb1
Erik Garrison <erik.garrison@gmail.com>
2025-08-29 09:23:43 +0000

    Double batch size to 128

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -73,7 +73,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --grad_clip 0.0 \
   --grad_accum 8 \
   --chunk_size 128 \
-  --batch_size 64 \
+  --batch_size 128 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
