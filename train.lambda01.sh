709bfeb0516b7c698e4ac0dd566617fbbdacfe90
Erik Garrison <erik.garrison@gmail.com>
2025-09-04 23:11:06 +0000

    Set batch_size to 64 and chunk_size to 1024

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -72,8 +72,8 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --weight_decay 0.0001 \
   --grad_clip 0.0 \
   --grad_accum 8 \
-  --chunk_size 2048 \
-  --batch_size 32 \
+  --chunk_size 1024 \
+  --batch_size 64 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
