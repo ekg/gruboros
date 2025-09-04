303894701fbf5a5914d93d116abdba27902f3f9b
Erik Garrison <erik.garrison@gmail.com>
2025-09-04 19:02:01 +0000

    Set batch_size to 32 and grad_accum to 1

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -71,9 +71,9 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --sf_beta2 0.995 \
   --weight_decay 0.0001 \
   --grad_clip 0.0 \
-  --grad_accum 8 \
+  --grad_accum 1 \
   --chunk_size 2048 \
-  --batch_size 64 \
+  --batch_size 32 \
   --keep_checkpoints 5 \
   --keep_elite 32 \
   --archive_rate 0.0067 \
