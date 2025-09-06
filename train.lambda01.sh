692d42d0b1d8c50ecb53404bf28fd7d6dedde959
Erik Garrison <erik.garrison@gmail.com>
2025-09-06 17:13:49 +0000

    Set grad_accum to 1

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -71,7 +71,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --sf_beta2 0.995 \
   --weight_decay 0.0001 \
   --grad_clip 0.0 \
-  --grad_accum 8 \
+  --grad_accum 1 \
   --chunk_size 1024 \
   --batch_size 96 \
   --keep_checkpoints 5 \
