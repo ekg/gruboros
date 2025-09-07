39f9031ec23bbbc26aed6b48134692ae32d250b1
Erik Garrison <erik.garrison@gmail.com>
2025-09-07 19:31:32 +0000

    Set dim to 2048

--- train.lambda01.sh
+++ train.lambda01.sh
@@ -61,7 +61,7 @@ torchrun --nproc_per_node=$NUM_GPUS \
   --data "$DATA_PATH" \
   --output "$OUTPUT_DIR" \
   --params $PARAMS \
-  --dim 1024 \
+  --dim 2048 \
   --expansion_factor 1.0 \
   --ff_mult 1.0 \
   --train_steps 10000000 \
